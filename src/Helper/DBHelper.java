d2732b70b6f6de790adf9e04fbadb611b647b55b
Ali Katırcıo <ali.katircio@gmail.com>
2017-12-15 11:42:59 +0300

    Update DBHelper.java

--- src/Helper/DBHelper.java
+++ src/Helper/DBHelper.java
@@ -4,7 +4,7 @@ public class DBHelper {
 
 	public static String dbUrl = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
 	public static String userName = "ALI";
-	public static String password = "a.1993.1907";
+	public static String password = "********";
 	public static String teams = "select * from teams order by id asc";
 	public static String gruplar = "select * from grup";
 	public static String torbalar = "select * from torba order by team_id asc";
